a9c39ed999b039ce21dc80624ffbed06efb1e811
Leonardorossato <leonardorossato21@gmail.com>
2021-05-01 10:18:31 -0300

    fazendo a rota de pagamento.

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,6 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const payment = require('../models/paymentModel')
 
+router.get('/', (req, res)=>{
+    payment.find((err, data)=>{
+        if(err){
+            return res.status(500).json('Some error occurred to search all payments',  err)
+        }
 
+        return res.status(200).json('Payments successfully reloaded', data)
+    })
+})
+
+router.post('/add/payment', (req, res)=>{
+    try{
+        const _payment = new Payment({
+            name: req.body.name,
+            month: Number(req.body.month),
+            year: Number(req.body.year),
+            credits: [],
+            debits: []
+        })
+
+        payment.findOne().where(_payment).exec((err, data)=>{
+            if(err){
+                return res.status(400).json('Error to add a new Payment', err)
+            }
+            if(data === null){
+                _payment.save((err, data)=>{
+                    if(err){
+                        return res.status(500).json('Error to add a new Payment',err)
+                    }
+                    return res.status(201).json('Payment added successfully', data)
+                })
+            }
+        })
+    }catch(err){
+        console.log(error)
+        throw Error()
+    }
+})
 
 module.exports = router;
