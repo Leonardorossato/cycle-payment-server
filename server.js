fc8d16d64f3314b94ce2cec623173ea2539271c4
Leonardorossato <leonardorossato21@gmail.com>
2021-05-02 11:19:28 -0300

    iimport do summaruRoutes.

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const paymentRoutes = require('./routes/paymentRoutes.js')
 const countRoutes = require('./routes/countRoutes.js')
+const sumarryRoutes = require('./routes/summaryRoutes')
 const PORT = 8000 || process.env.PORT
 const connectionMongoDB = require('./config/connectionMongoDB');
 
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api', paymentRoutes);
 app.use('/api', countRoutes)
+app.use('/api', sumarryRoutes)
 
 
 app.connectionMongoDB = connectionMongoDB
