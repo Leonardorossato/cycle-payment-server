9e225f6bbece57bcbfdbf361da435ab61967acb0
Leonardorossato <leonardorossato21@gmail.com>
2021-05-02 10:21:15 -0300

    ok.

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const router = require('./routes')(app)
+const paymentRoutes = require('./routes/paymentRoutes.js')
 const PORT = 8000 || process.env.PORT
 const connectionMongoDB = require('./config/connectionMongoDB');
 
 
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api', paymentRoutes);
+
+
 app.connectionMongoDB = connectionMongoDB
-app.router = router;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
