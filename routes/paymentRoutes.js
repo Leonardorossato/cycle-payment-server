f3bf66dad9c5a9e427998f23cb62672127982c83
Leonardorossato <leonardorossato21@gmail.com>
2021-05-02 10:20:59 -0300

    rota ajustada e funcionando. get e post.

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,52 +1,45 @@
 const express = require('express');
-const router = express.Router();
 const payment = require('../models/paymentModel')
+const router = express.Router();
 
 router.get('/', (req, res)=>{
-    payment.find((err, data)=>{
+    payment.find((err, data) =>{
         if(err){
-            return res.status(500).json('Some error occurred to search all payments',  err)
+            return res.status(500).json('Error to search the payments', err)
         }
-
-        return res.status(200).json('Payments successfully reloaded', data)
+        return res.status(200).json('Payment find successful : ' +data)
     })
 })
 
-router.post('/add/payment', (req, res)=>{
-    try{
-        const _payment = new Payment({
-            name: req.body.name,
-            month: Number(req.body.month),
-            year: Number(req.body.year),
-            credits: [{
-                name: req.body.name,
-                value: Number(req.body.value)
-            }],
-            debits: [{
-                name: req.body.name,
-                value: Number(req.body.value),
-                status: req.body.status,
-                enum: ['PAIDOUT', 'PENDENT', 'SCHEDULED']
-            }]
-        })
+router.post('/add', (req, res)=>{
+    const name = req.body.name;
+    const month = (req.body.month);
+    const year = (req.body.year);
+    const credits = [{
+        name: req.body.name,
+        value: req.body.value
+    }],
+    debits = [{
+        name: req.body.name,
+        value: req.body.value,
+        status: req.body.status
+    }]
 
-        payment.findOne().where(_payment).exec((err, data)=>{
-            if(err){
-                return res.status(400).json('Error to add a new Payment', err)
-            }
-            if(data === null){
-                _payment.save((err, data)=>{
-                    if(err){
-                        return res.status(500).json('Error to add a new Payment',err)
-                    }
-                    return res.status(201).json('Payment added successfully', data)
-                })
-            }
-        })
-    }catch(err){
-        console.log(error)
-        throw Error()
-    }
+    const newPayment = new payment({
+        name,
+        month,
+        year,
+        credits:[],
+        debits:[]       
+    });
+
+    newPayment.save()
+    .then(() => {
+        res.status(201).json('New payment added successfuly')
+    })
+    .catch((error) => {
+        res.status(400).json('Error to add a new payment' + error)
+    })
 })
 
 module.exports = router;
