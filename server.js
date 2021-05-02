0219553eda93775049de3718b63e196e4cd283a8
Leonardorossato <leonardorossato21@gmail.com>
2021-05-02 10:41:14 -0300

    rota count adicionada.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const paymentRoutes = require('./routes/paymentRoutes.js')
+const countRoutes = require('./routes/countRoutes.js')
 const PORT = 8000 || process.env.PORT
 const connectionMongoDB = require('./config/connectionMongoDB');
 
@@ -10,6 +11,7 @@ const connectionMongoDB = require('./config/connectionMongoDB');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', paymentRoutes);
+app.use('/api', countRoutes)
 
 
 app.connectionMongoDB = connectionMongoDB
--- service/paymentService.js
+++
@@ -1,7 +0,0 @@
-const paymentModel = require("../models/paymentModel")
-
-
-paymentModel.methods(['get', 'post', 'put', 'delete'])
-paymentModel.updateOptions({new: true, runValidators: true})
-
-module.exports = paymentModel
